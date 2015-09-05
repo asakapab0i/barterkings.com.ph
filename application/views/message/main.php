b67c6342b64289346e48538843e74e499b60922c
bryan <bojorquebryan@gmail.com>
2015-09-05 14:15:12 +0800

    fix message template

--- application/views/message/main.php
+++ application/views/message/main.php
@@ -1,251 +1,61 @@
 <div class="container">
   <ul class="nav nav-tabs" role="tablist">
-  <li class="">
+    <li class="">
       <a type="button" href="#compose" role="tab" data-toggle="tab"> 
         <span class="glyphicon glyphicon-pencil"></span> Compose
       </a>
     </li>
-    <li class="active">
-      <a href="#inbox" role="tab" data-toggle="tab">
-        Inbox <span class="label label-success">10</span>
-      </a>
-    </li>
-    <li><a href="#sent-mail" role="tab" data-toggle="tab">Sent mail</a>
-
+    <li class="active"><a href="#inbox" role="tab" data-toggle="tab">
+      Inbox <span class="label label-success">10</span>
+    </a></li>
+    <li><a href="#sent-mail" role="tab" data-toggle="tab">Sent Message</a>
     </li>
     <li><a href="#draft" role="tab" data-toggle="tab">Draft</a>
-
     </li>
     <li><a href="#trash" role="tab" data-toggle="tab">Trash</a>
-
     </li>
   </ul>
 </div>
 
 <div class="tab-content">
-  <div class="tab-pane active" id="inbox">
-
+  <div class="tab-pane" id="compose">
     <div class="container">
-     <div class="content-container clearfix">
-       <div class="col-md-12">
-         <h1 class="content-title">Inbox</h1>
-
-         <input type="search" placeholder="Search Mail" class="form-control mail-search" />
-
-         <ul class="mail-list">
-           <li>
-             <a href="">
-               <span class="mail-sender">You Tube</span>
-               <span class="mail-subject">New subscribers!</span>
-               <span class="mail-message-preview">You have ten more subscriptions click her...</span>
-             </a>
-           </li>
-           <li>
-             <a href="">
-               <span class="mail-sender">You Tube</span>
-               <span class="mail-subject">New subscribers!</span>
-               <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-             </a>
-           </li>
-           <li>
-             <a href="">
-               <span class="mail-sender">You Tube</span>
-               <span class="mail-subject">New subscribers!</span>
-               <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-             </a>
-           </li>
-           <li>
-             <a href="">
-               <span class="mail-sender">You Tube</span>
-               <span class="mail-subject">New subscribers!</span>
-               <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-             </a>
-           </li>
-           <li>
-             <a href="">
-               <span class="mail-sender">You Tube</span>
-               <span class="mail-subject">New subscribers!</span>
-               <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-             </a>
-           </li>
-         </ul>
-       </div>
-     </div>
-   </div>
-
- </div>
- <div class="tab-pane" id="compose">
-
-  <div class="container">
-    <div class="content-container clearfix">
-      <h1 class="content-title">Compose</h1>
-      <div class="col-md-12">
-        <div class="form-group">
-          <input id="tokenfield" type="text" class="form-control" placeholder="To" />
-        </div>
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Subject" />
-        </div>
-        <textarea class="form-control" placeholder="message"></textarea>
-        <div class="btn-send">
-          <button class="form-control btn btn-success">Send</button>
-        </div>
+      <div class="content-container clearfix">
+        <h1 class="content-title">Compose</h1>
+        <?php $this->load->view('template/message-compose-template'); ?>
       </div>
     </div>
   </div>
-
+  <div class="tab-pane active" id="inbox">
+   <div class="container">
+    <div class="content-container clearfix">
+    <h1 class="content-title">Inbox</h1>
+      <?php $this->load->view('template/message-template'); ?>
+    </div>
+  </div>
 </div>
 <div class="tab-pane" id="sent-mail">
-
-  <div class="container">
-   <div class="content-container clearfix">
-     <div class="col-md-12">
-       <h1 class="content-title">Sent Mail</h1>
-
-       <input type="search" placeholder="Search Mail" class="form-control mail-search" />
-
-       <ul class="mail-list">
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">You have ten more subscriptions click her...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-       </ul>
-     </div>
-   </div>
- </div>
-
+ <div class="container">
+  <div class="content-container clearfix">
+    <h1 class="content-title">Sent Message</h1>
+    <?php $this->load->view('template/message-template'); ?>
+  </div>
+</div>
 </div>
 <div class="tab-pane" id="draft">
-
   <div class="container">
-   <div class="content-container clearfix">
-     <div class="col-md-12">
-       <h1 class="content-title">Draft</h1>
-
-       <input type="search" placeholder="Search Mail" class="form-control mail-search" />
-
-       <ul class="mail-list">
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">You have ten more subscriptions click her...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-       </ul>
-     </div>
-   </div>
- </div>
-
+    <div class="content-container clearfix">
+      <h1 class="content-title">Draft</h1>
+      <?php $this->load->view('template/message-template'); ?>
+    </div>
+  </div>
 </div>
 <div class="tab-pane" id="trash">
-
-  <div class="container">
-   <div class="content-container clearfix">
-     <div class="col-md-12">
-       <h1 class="content-title">Trash</h1>
-
-       <input type="search" placeholder="Search Mail" class="form-control mail-search" />
-
-       <ul class="mail-list">
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">You have ten more subscriptions click her...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-         <li>
-           <a href="">
-             <span class="mail-sender">You Tube</span>
-             <span class="mail-subject">New subscribers!</span>
-             <span class="mail-message-preview">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet ipsum nisi? Eaque odio quae debitis saepe explicabo alias sit tenetur animi...</span>
-           </a>
-         </li>
-       </ul>
-     </div>
-   </div>
- </div>
-
+ <div class="container">
+  <div class="content-container clearfix">
+    <h1 class="content-title">Trash</h1>
+    <?php $this->load->view('template/message-template'); ?>
+  </div>
+</div>
 </div>
 </div>
---
+++ application/views/template/message-compose-template.php
@@ -0,0 +1,12 @@
+<div class="col-md-12">
+  <div class="form-group">
+    <input id="tokenfield" type="text" class="form-control" placeholder="To" />
+  </div>
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Subject" />
+  </div>
+  <textarea class="form-control" placeholder="message"></textarea>
+  <div class="btn-send">
+    <button class="form-control btn btn-success">Send</button>
+  </div>
+</div>
