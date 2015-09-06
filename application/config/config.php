16379c1d91665832a38aaddad8a141ce58473384
bryan <bojorquebryan@gmail.com>
2015-09-06 11:55:14 +0800

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+\<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://shielded-wave-3616.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
