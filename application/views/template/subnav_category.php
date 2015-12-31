0d898b870aa7362d1123224aec9d63ef49441045
Bryan Bojorque <bojorquebryan@gmail.com>
2016-01-01 04:54:26 +0800

    sub nav

---
+++ application/views/template/subnav_category.php
@@ -0,0 +1,123 @@
+  <div class="container">
+                <div class="container-fluid">
+                  <ul class="nav navbar-nav category-list">
+                    <li class="">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" type="button" id="cat1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Computer Electronics</a>
+
+                        <ul class="category-dropdown dropdown-menu clear" aria-labelledby="cat1">
+
+                          <li><a href="#">Cellphones & Accessories</a></li>
+                          <li><a href="#">Computers & Accessories</a></li>
+                          <li><a href="#">Gadgets & Accessories</a></li>
+                          <li><a href="#">Household Appliances & Accessories</a></li>
+                          <li><a href="#">Photography Equipment & Accessories</a></li>
+                          <li><a href="#">Other Electronic Devices</a></li>
+                        </ul>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" type="button" id="cat2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Entertainment & Hobbies</a>
+                        <ul class="category-dropdown dropdown-menu clear" aria-labelledby="cat2">
+                          <li><a href="#">Books & Magazines</a></li>
+                          <li><a href="#">Collectibles, Memorabilias & Hobbies</a></li>
+                          <li><a href="#">Music & Movies</a></li>
+                        </ul>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" id="cat3">Food & Beverages</a>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" type="button" id="cat4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Jobs</a>
+                        <ul class="category-dropdown dropdown-menu" aria-labelledby="cat4">
+                          <li><a href="#">Call Center / BPO</a></li>
+                          <li><a href="#">Overseas</a></li>
+                          <li><a href="#">Part-time & Home-based</a></li>
+                        </ul>
+                      </div>
+
+                    </li>
+                     <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" type="button" id="cat5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Lifestyle</a>
+                        <ul class="category-dropdown dropdown-menu" aria-labelledby="cat5">
+                          <li><a href="#">Babies & Kids Stuffs</a></li>
+                          <li><a href="#">Clothing & Accessories</a></li>
+                          <li><a href="#">Health & Beauty</a></li>
+                          <li><a href="#">Shoes, Bags & Accessories</a></li>
+                        </ul>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" id="cat6">Pets</a>
+                      </div>
+
+                    </li>
+                     <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle"id="cat7">Real Estate</a>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" type="button" id="cat8" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Specialty Services</a>
+                        <ul class="category-dropdown dropdown-menu" aria-labelledby="cat8">
+                          <li><a href="#">Parties & Events Coordination</a></li>
+                          <li><a href="#">Travel & Hospitality</a></li>
+                        </ul>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" id="cat9">Sporting Goods</a>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each dropdown-toggle" type="button" id="cat10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Vehicles, Parts, & Accessories</a>
+                        <ul class="category-dropdown dropdown-menu" aria-labelledby="cat10">
+                          <li><a href="#">Cars & Van</a></li>
+                          <li><a href="#">Pickup & SUVs</a></li>
+                          <li><a href="#">Tracks, Busses, & Service Vehicles</a></li>
+                          <li><a href="#">Auto Parts, Accessories, & Services</a></li>
+                          <li><a href="#">Motorcycles</a></li>
+                          <li><a href="#">Motorcycles Parts, Accesories, & Services</a></li>
+                          <li><a href="#">Common Grounds - Car Audio Gadgets</a></li>
+                        </ul>
+                      </div>
+
+                    </li>
+                    <li class="#">
+                  
+                      <div class="dropdown">
+                        <a href="#" class="category-each" id="cat11" >Other</a>
+                      </div>
+
+                    </li>
+
+                  </ul>
+                </div>
+                </div>
