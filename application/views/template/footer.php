ef4fd0820f2e6f837549e908dc9c66cc034eba48
Bryan Bojorque <bojorquebryan@gmail.com>
2016-04-30 15:34:00 +0800

    analytics

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -88,5 +88,12 @@
 <script src="<?php echo base_url('asset/js'); ?>/add-item.js"></script>
 <script src="<?php echo base_url('asset/js'); ?>/items-functionalities.js"></script>
 
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-77164709-1');ga('send','pageview');
+
 </body>
 </html>
