1c5b1cd9282f96d81cb7796c1a87e2f591f8c792
Bryan Bojorque <bojorquebryan@gmail.com>
2016-05-05 19:26:55 +0800

    remove comments

--- application/views/account/profile.php
+++ application/views/account/profile.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="col-md-12">
-        
+
         <div class="panel panel-default">
             <div class="panel-body reload-profile">
-                <?php $this->load->view('template/profile-template'); ?> 
+                <?php $this->load->view('template/profile-template'); ?>
             </div>
         </div>
-        
+
     </div>
         <div class="col-md-12">
             <div class="panel panel-default">
@@ -17,25 +17,5 @@
                     <?php $this->load->view('template/items-template'); ?>
                 </div>
             </div>
-        </div>
-        <!-- <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">My offers</h4>
-                </div>
-                <div class="panel-body">
-                    <?php $this->load->view('template/items-template'); ?>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">Offers to my item</h4>
-                </div>
-                <div class="panel-body">
-                    <?php $this->load->view('template/items-template'); ?>
-                </div>
-            </div>
-        </div> -->
+        </div
 </div>
