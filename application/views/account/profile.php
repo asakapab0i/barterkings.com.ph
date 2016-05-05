2422afd87df031a41dae872e3a76f10f680374ad
Bryan Bojorque <bojorquebryan@gmail.com>
2016-05-05 19:30:04 +0800

    profile

--- application/views/account/profile.php
+++ application/views/account/profile.php
@@ -1,21 +1,21 @@
 <div class="container">
-    <div class="col-md-12">
+  <div class="col-md-12">
 
-        <div class="panel panel-default">
-            <div class="panel-body reload-profile">
-                <?php $this->load->view('template/profile-template'); ?>
-            </div>
-        </div>
-
+    <div class="panel panel-default">
+      <div class="panel-body reload-profile">
+        <?php $this->load->view('template/profile-template'); ?>
+      </div>
     </div>
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">My listings</h4>
-                </div>
-                <div class="panel-body">
-                    <?php $this->load->view('template/items-template'); ?>
-                </div>
-            </div>
-        </div
+
+  </div>
+  <div class="col-md-12">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">My listings</h4>
+      </div>
+      <div class="panel-body">
+        <?php $this->load->view('template/items-template'); ?>
+      </div>
+    </div>
+  </div>
 </div>
