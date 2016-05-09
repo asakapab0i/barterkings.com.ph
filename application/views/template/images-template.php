6039740fd38a1ae06957161acf3d6a340ef3f798
Bryan Bojorque <bojorquebryan@gmail.com>
2016-05-09 12:05:18 +0800

    images

--- application/views/template/images-template.php
+++ application/views/template/images-template.php
@@ -1,10 +1,15 @@
 <?php if($images !== FALSE): ?>
 	<?php foreach($images as $image): ?>
 		<?php $name = $image['image_thumb'];  $large = $image['image']?>
+		<?php $is_default = ($name == 'default_thumb.png' ? '' : 'data-gallery'); ?>
 		<div class="col-md-3">
-			<a href="<?php echo base_url("asset/img/items/$large"); ?>" title="<?php echo url_title($name); ?>" data-gallery>
+			<?php if($is_default != ''): ?>
+			<a href="<?php echo base_url("asset/img/items/$large"); ?>" title="<?php echo url_title($name); ?>" <?php echo $is_default; ?>>
 				<img src="<?php echo base_url("asset/img/items_thumbs/$name"); ?>" alt="<?php echo url_title($name); ?>" class="thumbnail img-responsive img-thumbnail">
 			</a>
+			<?php else:?>
+				<img src="<?php echo base_url("asset/img/items_thumbs/$name"); ?>" alt="<?php echo url_title($name); ?>" class="thumbnail img-responsive img-thumbnail">
+			<?php endif;?>
 		</div>
 	<?php endforeach;?>
 <?php else: ?>
