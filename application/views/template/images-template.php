bc99c249a22e447b95984437efd6b03040aff46c
Bryan Bojorque <bojorquebryan@gmail.com>
2016-05-09 12:08:38 +0800

    images v2

--- application/views/template/images-template.php
+++ application/views/template/images-template.php
@@ -5,10 +5,10 @@
 		<div class="col-md-3">
 			<?php if($is_default != ''): ?>
 			<a href="<?php echo base_url("asset/img/items/$large"); ?>" title="<?php echo url_title($name); ?>" <?php echo $is_default; ?>>
-				<img src="<?php echo base_url("asset/img/items_thumbs/$name"); ?>" alt="<?php echo url_title($name); ?>" class="thumbnail img-responsive img-thumbnail">
+				<img src="<?php echo base_url("asset/img/items_thumbs/$name"); ?>" alt="<?php echo url_title($name); ?>" class="img-product thumbnail img-responsive img-thumbnail">
 			</a>
 			<?php else:?>
-				<img src="<?php echo base_url("asset/img/items_thumbs/$name"); ?>" alt="<?php echo url_title($name); ?>" class="thumbnail img-responsive img-thumbnail">
+				<img src="<?php echo base_url("asset/img/items_thumbs/$name"); ?>" alt="<?php echo url_title($name); ?>" class="img-product thumbnail img-responsive img-thumbnail">
 			<?php endif;?>
 		</div>
 	<?php endforeach;?>
